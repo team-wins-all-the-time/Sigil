54324c55c081346eb2d6385fbdd068940f325a09
Mary Cimakasky <MaryCimakasky@Marys-MBP.localdomain>
2019-02-25 11:52:25 -0800

    getting things ready for elementary testing

--- js/algrothim.js
+++ js/algrothim.js
@@ -1,8 +1,10 @@
 //global vars
   //user_string = user input;
   //user_array = [];
+
 //helper functions
 //random_number pulled from a previous project of mine.
+
 function random_number (max_num, min_num) {
   var max = Math.ceil(max_num);
   var min = Math.floor(min_num);
@@ -10,9 +12,13 @@ function random_number (max_num, min_num) {
 }
   //need to iterate through user string and turn it into individual letters.
   // then need to remove vowels and duplicate letters
+
 //user_array = array.from(user_string);
+//user_array.join(a,e,i,o,u)
+//arr.sort(user_array);
 //var individual_letters = user_array[Symbol.iterator]();
-//if individual_letters = a,e,i,o,u delete
+
+
 // else if individual_letters = duplicate, only keep first instance
 
 //constructor(necessary? will we just have the one object?)
@@ -43,8 +49,14 @@ var library ={
 };
 //when we have an array of unique consinents, compare them to the properties of library. when we find a match between a character and the character name of a property, select a random set of coordinates from the array and push taht to the array of "to be rendered"
 //when we have run through the entire array, select random starting coordinates for each shape, and render them to the canvas, uing any colors user has selected via radio buttons.
+
 //object instantnation(necessary?)
+
 //event lisiteners
   //submit button
+  
   //radio buttons
+
+//store rendered sigils to local storage
+
 //init
