bff3d6bf8edabb2f8e18ba8dfbeb134bc7eeecbf
Mary Cimakasky <MaryCimakasky@Marys-MBP.localdomain>
2019-02-25 11:03:15 -0800

    psudo and basic algorithim items

---
+++ js/algrothim.js
@@ -0,0 +1,12 @@
+//global vars
+  //user_string = user input
+//helper functions
+  //need to iterate through user string and turn it into individual letters. 
+  // then need to remove vowels and duplicate letters
+//var user_array = array.from(user_string);
+//var individual_letters = user_array[Symbol.iterator]();
+
+//constructors
+//object instantnation
+//event lisiteners
+//init
