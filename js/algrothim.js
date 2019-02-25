3064e8afe99e72b16fd82b98807aede5435cd515
Bonnie Wang <jcwang118@hotmail.com>
2019-02-25 11:53:19 -0800

    adding basics of split screen

---
+++ js/algrothim.js
@@ -0,0 +1,20 @@
+'use strict';
+var left = document.querySelector('.left');
+var right = document.querySelector('.right');
+var container = document.querySelector('.container');
+
+left.addEventListener('mouseenter',() => {
+  container.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave',() => {
+  container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter',() => {
+  container.classList.add('hover-right');
+});
+
+right.addEventListener('mouseleave',() => {
+  container.classList.remove('hover-right');
+});
