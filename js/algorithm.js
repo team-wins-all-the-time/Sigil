8edb7216766773e991cfdde1fa9c823c0b0f9b69
Mary Cimakasky <MaryCimakasky@Marys-MBP.localdomain>
2019-02-26 10:24:38 -0800

    fixed a persistant comma issue

--- js/algorithm.js
+++ js/algorithm.js
@@ -29,19 +29,20 @@ function random_number (max_num, min_num) {
 //remove duplicate letters and vowels, match individual letters up with library arrays and select shape at random
 
 function process_sigil(user_string) {
-  // console.log('begining of process_sigil',user_string);
+  console.log('begining of process_sigil',user_string);
   var user_array = Array.from(new Set(user_string.split('').map(letter => letter.toLowerCase())));
-  // console.log('after newSet and array.from', user_array);
+  console.log('after newSet and array.from', user_array);
   var to_remove = ['a','e','i','o','u','.','!',',',' '];
   user_array = user_array.filter(letter => !to_remove.includes(letter));
-  // console.log('after array.join', user_array);
+  console.log('after array.join', user_array);
   var sigil_array = [];
   for (var i = 0; i < user_array.length; i++){
-    // console.log(ctx.arc(150, 75, 25, 0, 2 * Math.PI));
-    var thing = eval(`library.${user_array[i]}`);
-    // console.log(user_array[i]);
+    // var thing = eval(`library.${user_array[i]}`);
+  var thing = library[user_array[i]];
+    console.log(user_array[i]);
     sigil_array.push(thing[random_number(1,0)]);
-    // console.log(random_number(1,0));
+    console.log(random_number(1,0));
+    console.log(thing);
 
   }
   return sigil_array;
@@ -55,38 +56,34 @@ function process_sigil(user_string) {
 
 //constructor(necessary? will we just have the one object?)
 var library ={
-  // b: [[[150, 75, 25, 0, 2], 'arc'], [[150, 75, 100, 0], 'rect']],
-  c: [[[150, 75, 25, 0, 2], 'arc'], [[150, 75, 100, 0], 'rect']],
-
-
   b: [[[150, 75, 25, 0, 2], 'arc'], [[150, 75, 100, 0], 'rect']],
-  // c: [ctx.arc(100, 50, 35, .60, 1 * Math.PI), ctx.rect(150, 75, 25, 50)],
-  // d: [ctx.arc(150, 75, 25, 0, .5 * Math.PI), ctx.rect(100, 150, 97, 80)],
-  // f: [ctx.arc(100, 50, 35, 0, 2 * Math.PI), ctx.rect(85, 20, 150, 150)],
-  // g: [ctx.arc(50, 100, 35, .80, 1 * Math.PI), ctx.rect(30, 100, 42, 77)],
-  // h: [ctx.arc(100, 50, 100, 0, .5 * Math.PI),ctx.rect(70, 20, 60, 27)],
-  // j: [ctx.arc(200, 45, 50, 0, 2 * Math.PI), ctx.rect(15, 101, 150, 13)],
-  // k: [ctx.arc(150, 70, 84, .5, 1 * Math.PI), ctx.rect(69, 6, 20, 2)],
-  // l: [ctx.arc(75, 100, 40, 23, .5 * Math.PI), ctx.rect(12, 59, 4, 31)],
-  // m: [ctx.arc(175, 50, 60, 0, 2 * Math.PI), ctx.rect(37, 57, 69, 41)],
-  // n: [ctx.arc(100, 150, 80, 0, 1 * Math.PI), ctx.rect(65, 1, 32, 129)],
-  // p: [ctx.arc(100, 50, 35, 55, .5 * Math.PI), ctx.rect(67, 59, 142, 64)],
-  // q: [ctx.arc(50, 200, 80, 0, 2 * Math.PI), ctx.rect(70, 104, 89, 120)],
-  // r: [ctx.arc(50, 35, 8, 23, 1 * Math.PI), ctx.rect(123, 135, 132, 19)],
-  // s: [ctx.arc(25, 130, 70, 35, .5 * Math.PI), ctx.rect(19, 113, 129, 54)],
-  // t: [ctx.arc(39, 100, 15, 0, 2 * Math.PI), ctx.rect(115, 89, 47, 139)],
-  // v: [ctx.arc(150, 150, 40, .15, 1 * Math.PI), ctx.rect(49, 93, 124, 147)],
-  // w: [ctx.arc(200, 75, 10, 86, .5 * Math.PI), ctx.rect(122, 69, 38, 3)],
-  // x: [ctx.arc(100, 85, 95, 0, 2 * Math.PI), ctx.rect(72, 32, 53, 16)],
-  // y: [ctx.arc(100, 100, 100, 0, 1 * Math.PI), ctx.rect(66, 86, 91, 11)],
-  // z: [ctx.arc(8, 2, 100, 100, .5 * Math.PI), ctx.rect(48, 140, 75, 128)],
+  c: [[[100, 50, 35, .60, 1], 'arc'], [[150, 75, 25, 50], 'rect']],
+  d: [[[150, 75, 25, 0, .5], 'arc'], [[100, 150, 97, 80], 'rect']],
+  f: [[[100, 50, 35, 0, 2], 'arc'], [[85, 20, 150, 150], 'rect']],
+  g: [[[50, 100, 35, .80, 1],'arc'], [[30, 100, 42, 77], 'rect']],
+  h: [[[100, 50, 100, 0, .5],'arc'], [[70, 20, 60, 27], 'rect']],
+  j: [[[200, 45, 50, 0, 2 ], 'arc'], [[15, 101, 150, 13], 'rect']],
+  k: [[[150, 70, 84, .5, 1], 'arc'], [[69, 6, 20, 2], 'rect']],
+  l: [[[75, 100, 40, 23, .5], 'arc'], [[12, 59, 4, 31], 'rect']],
+  m: [[[175, 50, 60, 0, 2], 'arc'], [[37, 57, 69, 41], 'rect']],
+  n: [[[100, 150, 80, 0, 1], 'arc'], [[65, 1, 32, 129], 'rect']],
+  p: [[[100, 50, 35, 55, .5], 'arc'], [[67, 59, 142, 64], 'rect']],
+  q: [[[50, 200, 80, 0, 2], 'arc'], [[70, 104, 89, 120], 'rect']],
+  r: [[[50, 35, 8, 23, 1], 'arc'], [[123, 135, 132, 19], 'rect']],
+  s: [[[25, 130, 70, 35, .5], 'arc'], [[19, 113, 129, 54], 'rect']],
+  t: [[[39, 100, 15, 0, 2], 'arc'], [[115, 89, 47, 139], 'rect']],
+  v: [[[150, 150, 40, .15, 1], 'arc'], [[49, 93, 124, 147], 'rect']],
+  w: [[[200, 75, 10, 86, .5], 'arc'], [[122, 69, 38, 3], 'rect']],
+  x: [[[100, 85, 95, 0, 2], 'arc'], [[72, 32, 53, 16], 'rect']],
+  y: [[[100, 100, 100, 0, 1], 'arc'], [[66, 86, 91, 11], 'rect']],
+  z: [[[8, 2, 100, 100, .5], 'arc'], [[48, 140, 75, 128], 'rect']],
 
 };
 
 function write_sigil(sigil_array){
   ctx.beginPath();
   for (var l = 0; l < sigil_array.length; l ++){
-
+    console.log(sigil_array);
     if (sigil_array[l][1] === 'rect'){
       ctx.rect(sigil_array[l][0][0], sigil_array[l][0][1], sigil_array[l][0][2], sigil_array[l][0][3]);
     }
@@ -149,7 +146,9 @@ form.addEventListener('submit',function (event){
 
   var sigil_array = process_sigil(user_string);
 
+  console.log(sigil_array);
   write_sigil(sigil_array);
+  
 });
 
 
